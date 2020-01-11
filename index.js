f4fef9a31e8be6c5acdcd3ad7083e000cbbfe361
beloved-format-3447 <enc.mk.89@gmail.com>
2020-01-11 03:02:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "greating" + specialGuest + "!";
+var greetSpecialGuest = "greeting" + specialGuest + "!";
 
 var topic = "space";
 
